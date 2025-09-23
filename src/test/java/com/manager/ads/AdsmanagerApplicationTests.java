a1dc749d0a5710d3415701a9a42c02d673051d40
Aditya More <more.aditya2001@gmail.com>
2025-09-23 17:30:35 +0530

    first commit

---
+++ src/test/java/com/manager/ads/AdsmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manager.ads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdsmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
