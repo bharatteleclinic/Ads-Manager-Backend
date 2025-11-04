a89a2c31e512d26e8a7526670a238fb82d84b306
Aditya More <more.aditya2001@gmail.com>
2025-11-04 12:08:08 +0530

    cost estimation

--- src/main/java/com/manager/ads/Controller/CostEstimationController.java
+++ src/main/java/com/manager/ads/Controller/CostEstimationController.java
@@ -111,41 +111,4 @@ public class CostEstimationController {
                 .contentType(MediaType.APPLICATION_PDF)
                 .body(pdfBytes);
     }
-<<<<<<< HEAD
 }
-=======
-}
-
-// package com.manager.ads.Controller;
-
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.http.HttpHeaders;
-// import org.springframework.http.MediaType;
-// import org.springframework.http.ResponseEntity;
-// import org.springframework.web.bind.annotation.PostMapping;
-// import org.springframework.web.bind.annotation.RequestBody;
-// import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.RestController;
-
-// import com.manager.ads.Entity.CostEstimationRequest;
-// import com.manager.ads.Service.CostEstimationPdfService;
-
-// @RestController
-// @RequestMapping("/api/cost-estimation") // ✅ Base path
-// public class CostEstimationController {
-
-//     @Autowired
-//     private CostEstimationPdfService costEstimationPdfService;
-
-//     @PostMapping("/preview")
-//     public ResponseEntity<byte[]> generatePreview(@RequestBody CostEstimationRequest request) throws Exception {
-//         // Generate PDF using the request payload (no DB dependency)
-//         byte[] pdfBytes = costEstimationPdfService.generatePreviewCostEstimation(request);
-
-//         return ResponseEntity.ok()
-//                 .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=cost_estimation_preview.pdf")
-//                 .contentType(MediaType.APPLICATION_PDF)
-//                 .body(pdfBytes);
-//     }
-// }
->>>>>>> db7a70c80e360a2db10d61e7963135d8d8970598
